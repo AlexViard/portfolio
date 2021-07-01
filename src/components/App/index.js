027dc4ed379bfae685b3f7a96d1497c124872724
AlexViard <alex-viard@hotmail.fr>
2021-07-01 22:21:13 +0200

    create components + Menu design

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,15 @@
 // == Import npm
 import React from 'react';
 
-// == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+// == Import components
+import Menu from 'src/components/Menu';
+
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Menu />
   </div>
 );
 
---
+++ src/components/Menu/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './style.scss';
+
+// import des Icons
+import {
+  IoHomeOutline,
+  IoPersonOutline,
+  IoSchoolOutline,
+  IoCodeSlashOutline,
+  IoLayersOutline,
+  IoChatbubblesOutline,
+  IoPaperPlaneOutline,
+} from 'react-icons/io5';
+
+const Menu = () => (
+  <div className="menu">
+    <nav className="menu-nav">
+      <ul>
+        <li className="menu-nav-li">
+          <IoHomeOutline />
+          <a className="menu-nav-li-link">Accueil</a>
+        </li>
+        <li className="menu-nav-li">
+          <IoPersonOutline />
+          <a className="menu-nav-li-link">À propos de moi</a>
+        </li>
+        <li className="menu-nav-li">
+          <IoSchoolOutline />
+          <a className="menu-nav-li-link">Experiences</a>
+        </li>
+        <li className="menu-nav-li">
+          <IoCodeSlashOutline />
+          <a className="menu-nav-li-link">Compétences</a>
+        </li>
+        <li className="menu-nav-li">
+          <IoLayersOutline />
+          <a className="menu-nav-li-link">Projets</a>
+        </li>
+        <li className="menu-nav-li">
+          <IoChatbubblesOutline />
+          <a className="menu-nav-li-link">Avis</a>
+        </li>
+        <li className="menu-nav-li">
+          <IoPaperPlaneOutline />
+          <a className="menu-nav-li-link">Me contacter</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Menu;
