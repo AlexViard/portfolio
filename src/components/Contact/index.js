d65a8e0dbcef7694360f9c27c50ac8dc5f40d266
AlexViard <alex-viard@hotmail.fr>
2021-08-07 18:12:52 +0200

    install dotenv

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -42,7 +42,7 @@ const Contact = () => {
 
           <div className="contact-content-container-card">
             <WrappedMap
-              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAP_KEY}`}
               loadingElement={<div style={{ height: '100%' }} />}
               containerElement={<div style={{ height: '100%' }} />}
               mapElement={<div style={{ height: '100%' }} />}
