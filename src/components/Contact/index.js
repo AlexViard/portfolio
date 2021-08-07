9e7d6c71330be0069cff6b7c45041c30f22670bc
AlexViard <alex-viard@hotmail.fr>
2021-08-07 17:56:12 +0200

    config googleMap

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
 import './style.scss';
 
@@ -23,13 +24,31 @@ const Contact = () => {
       });
   };
 
+  const map = () => (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 43.253790, lng: 2.264710 }}
+    />
+  );
+
+  const WrappedMap = withScriptjs(withGoogleMap(map));
+
   return (
     <div className="contact">
       <div className="contact-content">
         <h1 className="contact-content-title">Me contactez</h1>
 
         <div className="contact-content-container">
-          <div className="contact-content-container-card"></div>
+
+          <div className="contact-content-container-card">
+            <WrappedMap
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ height: '100%' }} />}
+              containerElement={<div style={{ height: '100%' }} />}
+              mapElement={<div style={{ height: '100%' }} />}
+            />
+          </div>
+
           <div className="contact-content-container-informations">
             <form className="contact-form" onSubmit={sendEmail}>
               <input type="text" name="firstName" placeholder="Prénom" />
