8ac51bf4bc7146f869eee00bb5516890601f23e0
AlexViard <alex-viard@hotmail.fr>
2021-08-18 02:32:26 +0200

    section mode light end + function change value of theme

--- src/components/ThemeSetting/index.js
+++ src/components/ThemeSetting/index.js
@@ -5,31 +5,49 @@ import {
 } from 'react-icons/io5';
 import './style.scss';
 
-const ThemeSetting = () => (
-  <div className="theme-setting">
-    <div className="theme-setting-icon">
-      <IoSettingsOutline />
-    </div>
-    <div className="theme-setting-menu">
-      <h1 className="theme-setting-menu-title">Thème</h1>
-      <div className="theme-setting-menu-color">
-        <div className="theme-setting-menu-color-item dark"></div>
-        <div className="theme-setting-menu-color-item light"></div>
+const ThemeSetting = () => {
+  const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+  };
+
+  return (
+    <div className="theme-setting">
+      <div className="theme-setting-icon">
+        <IoSettingsOutline />
       </div>
-      <h1 className="theme-setting-menu-title">Couleurs</h1>
-      <div className="theme-setting-menu-color">
-        <div style={{backgroundColor: '#BA2929'}} className="theme-setting-menu-color-item"></div>
-        <div style={{backgroundColor: '#059040'}} className="theme-setting-menu-color-item"></div>
-        <div style={{backgroundColor: '#08689E'}} className="theme-setting-menu-color-item"></div>
-        <div style={{backgroundColor: '#EB7524'}} className="theme-setting-menu-color-item"></div>
-        <div style={{backgroundColor: '#E7D21C'}} className="theme-setting-menu-color-item"></div>
-        <div style={{backgroundColor: '#8610B6'}} className="theme-setting-menu-color-item"></div>
-        <div style={{backgroundColor: '#54350A'}} className="theme-setting-menu-color-item"></div>
-        <div style={{backgroundColor: '#C1219C'}} className="theme-setting-menu-color-item"></div>
-        <div style={{backgroundColor: '#21C1BF'}} className="theme-setting-menu-color-item"></div>
+      <div className="theme-setting-menu">
+        <h1 className="theme-setting-menu-title">Thème</h1>
+        <div className="theme-setting-menu-color">
+          <div
+            className="theme-setting-menu-color-item dark"
+            onClick={() => {
+              setTheme('dark');
+            }}
+          >
+          </div>
+          <div
+            className="theme-setting-menu-color-item light"
+            onClick={() => {
+              setTheme('light');
+            }}
+          >
+          </div>
+        </div>
+        <h1 className="theme-setting-menu-title">Couleurs</h1>
+        <div className="theme-setting-menu-color">
+          <div style={{ backgroundColor: '#BA2929' }} className="theme-setting-menu-color-item"></div>
+          <div style={{ backgroundColor: '#059040' }} className="theme-setting-menu-color-item"></div>
+          <div style={{ backgroundColor: '#08689E' }} className="theme-setting-menu-color-item"></div>
+          <div style={{ backgroundColor: '#EB7524' }} className="theme-setting-menu-color-item"></div>
+          <div style={{ backgroundColor: '#E7D21C' }} className="theme-setting-menu-color-item"></div>
+          <div style={{ backgroundColor: '#8610B6' }} className="theme-setting-menu-color-item"></div>
+          <div style={{ backgroundColor: '#54350A' }} className="theme-setting-menu-color-item"></div>
+          <div style={{ backgroundColor: '#C1219C' }} className="theme-setting-menu-color-item"></div>
+          <div style={{ backgroundColor: '#21C1BF' }} className="theme-setting-menu-color-item"></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ThemeSetting;
